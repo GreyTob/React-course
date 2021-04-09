6cae82d21366f50ed0a7012742236ec1f9678a6e
Sergey <GreyTob@inbox.ru>
2021-04-09 20:47:43 +0500

    динамические списки

--- src/App.js
+++ src/App.js
@@ -19,10 +19,19 @@ class App extends Component {
     } )
   }
 
-  changeTitleHandler = newTitle => {
-    this.setState({
-      pageTitle: newTitle
-    })
+  onChangeName = (name, index) => {
+    console.log(name, index)
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({cars: cars})
+  }
+
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+    this.setState({ cars })
   }
 
   render() {
@@ -39,7 +48,8 @@ class App extends Component {
             key={index}
             name={car.name}
             year={car.year}
-            onChangeTitle={() => this.changeTitleHandler(car.name)}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={(event) => this.onChangeName(event.target.value, index)}
           />
         )
       })
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,24 +1,14 @@
 import React from 'react'
 
-// function Car () {
-//   return (
-//     <h3>This is car component</h3>
-//   )
-// }
-// const Car = () => {
-//   return <div>This is car component</div>
-// }
-// const Car = () => (
-//   <div>This is car component
-//     <strong> test</strong>
-//   </div>
-// )
-
-// eslint-disable-next-line import/no-anonymous-default-export
 export default props => (
-  <div>
+  <div style={ {
+    border: '1px solid #ccc',
+    marginBottom: '10px',
+    padding: '10px',
+  } }>
      <h3>Car name: {props.name}</h3>
      <p>Year: <strong>{props.year}</strong></p>
-    <button onClick={props.onChangeTitle}>Click</button>
+    <input type="text" onChange={props.onChangeName} value={props.name}/>
+    <button onClick={props.onDelete}>Delete</button>
   </div>
 )
