10dee4cc713c6fd718916adce5a2cb6da1837855
Sergey <GreyTob@inbox.ru>
2021-04-15 20:38:32 +0500

    parametr to component

--- src/App.js
+++ src/App.js
@@ -56,7 +56,8 @@ class App extends Component {
 
     return (
       <div className="App" style={st}>
-        <h1>{this.state.pageTitle}</h1>
+        {/*<h1>{this.state.pageTitle}</h1>*/}
+        <h1>{this.props.title}</h1>
 
         <button
           onClick={this.toggleCarsHandler}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title={'I am parametr from index.js '} />
   </React.StrictMode>,
   document.getElementById('root')
 );
